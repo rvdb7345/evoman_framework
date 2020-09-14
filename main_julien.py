024c6d1c2fdb4e4d558eabc918d9c3a5b2ad025d
Julien Fer <70455218+FerJRM@users.noreply.github.com>
2020-09-14 13:05:36 +0200

    tested simulation

--- main_julien.py
+++ main_julien.py
@@ -10,85 +10,85 @@ from heuristics import DistanceRank, DistanceRoulette, DistanceWeightedRank
 from heuristics import DistanceRankAdaptiveNpoint
 
 if __name__ == "__main__":
-    # sim = SimulationRank(
-    #     experiment_name="basic_sim_rank",
-    #     nr_inputs=20,
-    #     nr_layers=1,
-    #     nr_neurons=10,
-    #     nr_outputs=5,
-    #     activation_func=["sigmoid"],
-    #     activation_distr=[1],
-    #     lower_bound=-1,
-    #     upper_bound=1,
-    #     pop_size=10,
-    #     nr_gens=5,
-    #     mutation_chance=0.2,
-    #     nr_skip_parents=2,
-    #     enemies=[8],
-    #     multiplemode="no"
-    # )
-    # sim.run_evolutionary_algo()
-    # print("FINISHED!!!!")
+    sim = SimulationRank(
+        experiment_name="basic_sim_rank",
+        nr_inputs=20,
+        nr_layers=1,
+        nr_neurons=10,
+        nr_outputs=5,
+        activation_func=["sigmoid"],
+        activation_distr=[1],
+        lower_bound=-1,
+        upper_bound=1,
+        pop_size=10,
+        nr_gens=5,
+        mutation_chance=0.2,
+        nr_skip_parents=2,
+        enemies=[8],
+        multiplemode="no"
+    )
+    sim.run_evolutionary_algo()
+    print("FINISHED!!!!")
     
-    # sim = SimulationRoulette(
-    #     experiment_name="basic_sim_roulette",
-    #     nr_inputs=20,
-    #     nr_layers=1,
-    #     nr_neurons=10,
-    #     nr_outputs=5,
-    #     activation_func=["sigmoid"],
-    #     activation_distr=[1],
-    #     lower_bound=-1,
-    #     upper_bound=1,
-    #     pop_size=10,
-    #     nr_gens=5,
-    #     mutation_chance=0.2,
-    #     nr_skip_parents=2,
-    #     enemies=[8],
-    #     multiplemode="no"
-    # )
-    # sim.run_evolutionary_algo()
-    # print("FINISHED!!!!")
+    sim = SimulationRoulette(
+        experiment_name="basic_sim_roulette",
+        nr_inputs=20,
+        nr_layers=1,
+        nr_neurons=10,
+        nr_outputs=5,
+        activation_func=["sigmoid"],
+        activation_distr=[1],
+        lower_bound=-1,
+        upper_bound=1,
+        pop_size=10,
+        nr_gens=5,
+        mutation_chance=0.2,
+        nr_skip_parents=2,
+        enemies=[8],
+        multiplemode="no"
+    )
+    sim.run_evolutionary_algo()
+    print("FINISHED!!!!")
 
-    # sim = SimulationWeightedRank(
-    #     experiment_name="basic_weighted_rank",
-    #     nr_inputs=20,
-    #     nr_layers=1,
-    #     nr_neurons=10,
-    #     nr_outputs=5,
-    #     activation_func=["sigmoid"],
-    #     activation_distr=[1],
-    #     lower_bound=-1,
-    #     upper_bound=1,
-    #     pop_size=10,
-    #     nr_gens=5,
-    #     mutation_chance=0.2,
-    #     nr_skip_parents=2,
-    #     enemies=[8],
-    #     multiplemode="no"
-    # )
-    # sim.run_evolutionary_algo()
-    # print("FINISHED!!!!")
+    sim = SimulationWeightedRank(
+        experiment_name="basic_weighted_rank",
+        nr_inputs=20,
+        nr_layers=1,
+        nr_neurons=10,
+        nr_outputs=5,
+        activation_func=["sigmoid"],
+        activation_distr=[1],
+        lower_bound=-1,
+        upper_bound=1,
+        pop_size=10,
+        nr_gens=5,
+        mutation_chance=0.2,
+        nr_skip_parents=2,
+        enemies=[8],
+        multiplemode="no"
+    )
+    sim.run_evolutionary_algo()
+    print("FINISHED!!!!")
 
-    # sim = SimulationAdaptiveMutationNpointCrossover(
-    #     experiment_name="weighted_sim_npoint",
-    #     nr_inputs=20,
-    #     nr_layers=1,
-    #     nr_neurons=10,
-    #     nr_outputs=5,
-    #     activation_func=["sigmoid"],
-    #     activation_distr=[1],
-    #     lower_bound=-1,
-    #     upper_bound=1,
-    #     pop_size=10,
-    #     nr_gens=5,
-    #     mutation_chance=0.2,
-    #     nr_skip_parents=2,
-    #     enemies=[8],
-    #     multiplemode="no"
-    # )
-    # sim.run_evolutionary_algo()
-    # print("FINISHED!!!!")
+    sim = SimulationAdaptiveMutationNpointCrossover(
+        experiment_name="weighted_sim_npoint",
+        nr_inputs=20,
+        nr_layers=1,
+        nr_neurons=10,
+        nr_outputs=5,
+        activation_func=["sigmoid"],
+        activation_distr=[1],
+        lower_bound=-1,
+        upper_bound=1,
+        pop_size=10,
+        nr_gens=5,
+        mutation_chance=0.2,
+        nr_skip_parents=2,
+        enemies=[8],
+        multiplemode="no"
+    )
+    sim.run_evolutionary_algo()
+    print("FINISHED!!!!")
 
     sim = DistanceRank(
         experiment_name="basic_distance_rank",
