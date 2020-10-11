25c2e48260fb4374d000b1bc910e2cd370971082
FerJRM <70455218+FerJRM@users.noreply.github.com>
2020-10-11 22:01:39 +0200

    merged analyses best solution

--- analyse_sols.py
+++ analyse_sols.py
@@ -10,11 +10,26 @@ This code was implemented by
 Julien Fer, Robin van den Berg, ...., ....
 """
 
+# insert evoman framework to path
+import sys
+sys.path.insert(0, "evoman")
+
 import os
 import csv
-import numpy as np
+import time
+from multiprocessing import Pool, cpu_count
 
-def load_best_sols(experiment_name, filename_sols, filename_fits):
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import ttest_ind, levene, f_oneway
+
+from environment import Environment
+from demo_controller import player_controller
+
+# set style sheet matplotlib
+plt.style.use("seaborn-darkgrid")
+
+def load_best_sols(experiment_name, filename_sols, filename_fits, N):
     """
     Loads best solutions with their corresponding best fitness of 
     Monte Carlo simulations into a ??? . Also determines the best solution and 
@@ -29,58 +44,255 @@ def load_best_sols(experiment_name, filename_sols, filename_fits):
     best_fits = []
     with open(relpath_fits, 'r') as csv_file:
         reader = csv.reader(csv_file)
-        best_fits = [float(row[0]) for row in reader]
+        new_best_sols = []
+        for i, row in enumerate(reader):
+            best_fits.append(float(row[0]))
+            if i < total_sols:
+                new_best_sols.append(best_sols["arr_{}".format(i)]) 
         best_fits = best_fits[1:]
+        best_sols = new_best_sols
 
     # find best solution
     best_fit_idx = np.argmax(best_fits)
     best_fit = best_fits[best_fit_idx]
-    best_sol = best_sols["arr_" + str(best_fit_idx)]
+    best_sol = best_sols[best_fit_idx]
 
-    # find solution with max distance and with minimum difference in best fitness
+    # find solution with max distance and hopfully different behavior
     max_poss_distance = len(best_sol) * 2
     distances, max_distance, idx_max_distance = [], 0, 0
-    difference_fits, min_diff_fit, idx_mindiff_fit = [], np.inf, 0
     for sol in range(total_sols):
 
         # determine distance and difference in fitness
-        other_sol = best_sols["arr_" + str(sol)]
+        other_sol = best_sols[sol]
         distance = np.linalg.norm(best_sol - sol) / max_poss_distance
-        other_fit = best_fits[sol]
-        diff_fit = best_fit - other_fit
 
         # found "more different" solution
         if distance > max_distance:
             max_distance = distance
             idx_max_distance = sol
 
-        # found different but relatively better solution w.r.t other nonbest solutios
-        if diff_fit < min_diff_fit and sol != best_fit_idx:
-            min_diff_fit = diff_fit
-            idx_mindiff_fit = sol
-
-        distances.append(distance), difference_fits.append(diff_fit)
+        distances.append(distance)
     
     # most "different" solution
-    sol_maxdistance = best_sols["arr_" + str(idx_max_distance)]
-    
-    # only keep the second best solution if it is different enough
-    sol_mindiff_fit = None
-    if distances[idx_mindiff_fit] > 0.5:
-        sol_mindiff_fit = best_sols["arr_" + str(idx_mindiff_fit)]
+    sol_maxdistance = best_sols[idx_max_distance]
+
+    # (bubble) sort solutions based on their fitness and select N best solutions
+    i, swaps = 0, np.inf
+    while (i < total_sols - 1 and swaps > 0):
+        swaps = 0
+        for j in range(i + 1, total_sols):
+            if best_fits[i] > best_fits[j]:
+                temp = best_fits[i]
+                best_fits[i] = best_fits[j]
+                best_fits[j] = temp
+
+                temp = best_sols[i]
+                best_sols[i] = best_sols[j]
+                best_sols[j] = temp
+
+                swaps += 1
+        i += 1
+
+    best_N_sols = best_sols[-N:]
 
     # returns best solution, most different solution and second best solution
-    return best_sol, sol_maxdistance, sol_mindiff_fit
+    return best_sol, sol_maxdistance, best_N_sols
+
+def play_game(name, pcont, enemies, multiplemode, seed):
+    """
+    Helper function to simulate a game in the Evoman framework
+    """
+    np.random.seed(seed)
+    env = Environment(
+        experiment_name=name,
+        enemies=enemies,
+        multiplemode=multiplemode,
+        playermode="ai",
+        player_controller=player_controller(10),
+        enemymode="static",
+        level=2,
+        speed="fastest",
+        logs="off"
+    )
+
+    return env.play(pcont=pcont)
+
+def run_solutions(name, N, solutions):
+    """
+    Run a certain solutions against all the enemies in the EvoMan framework
+    """
+
+    # run the given solutions against all enemies in parallel
+    enemies = [1, 2, 3, 4, 5, 6, 7, 8]
+    total_sols, total_enemies = len(solutions), len(enemies)
+    seed, pool_input = 0, []
+    for sol in solutions:
+        for _ in range(N):
+            for enemy in enemies:
+                pool_input.append((name, sol, [enemy], "no", seed))
+                seed += 1
+
+    pool = Pool(cpu_count())
+    pool_results = pool.starmap(play_game, pool_input)
+    pool.close()
+    pool.join()
+
+    # get the statistics
+    fitnesses, player_lifes, enemies_lifes = [], [], []
+    for results in pool_results:
+        fitness, player_life, enemy_life, _ = results
+        fitnesses.append(fitness)
+        player_lifes.append(player_life), enemies_lifes.append(enemy_life)
+    
+    # mean and max fitness per enemy
+    column_per_enemy_fitness = np.reshape(fitnesses, (total_sols, N, total_enemies))
+    mean_fitness_per_enemy = np.mean(column_per_enemy_fitness, axis=(0,1))
+    max_fitness_per_enemy = np.amax(column_per_enemy_fitness, axis=(0,1))
+
+    # determine individual gain for all runs
+    player_lifes = np.reshape(player_lifes, (total_sols, N, total_enemies))
+    enemies_lifes = np.reshape(enemies_lifes, (total_sols, N, total_enemies))
+    column_per_enemy_indgain = player_lifes - enemies_lifes
+    mean_indgain_per_sol_enemy = np.mean(column_per_enemy_indgain, axis=1)
+
+    # mean total individual game per solution
+    sum_indgain_per_sol = np.sum(column_per_enemy_indgain, axis=2)
+    mean_indgain_per_sol = np.mean(sum_indgain_per_sol, axis=1)
+    idx_best = np.argmax(mean_indgain_per_sol)
+    best_solution = solutions[idx_best]
+    best_indgain_per_enemy = mean_indgain_per_sol_enemy[idx_best, :]
+    print("Best solution has index: ", np.argmax(mean_indgain_per_sol))
+
+    print("This should be the average summed individual gain per solution: \n",
+          mean_indgain_per_sol)
+
+    # save best solution
+    folder = os.path.join("results", "runs_best_solutions")
+    os.makedirs(folder, exist_ok=True)
+    relpath = os.path.join(folder, name + "_bestgain.npy")
+    np.save(relpath, best_solution)
+    relpath = os.path.join(folder, name + "_bestgain_perenemy.npy")
+    np.save(relpath, best_indgain_per_enemy)
+
+    return column_per_enemy_fitness, column_per_enemy_indgain, mean_indgain_per_sol, best_solution, best_indgain_per_enemy
+
+def statistical_tests(
+        mean_indgain_dgea1, mean_indgain_dgea2, 
+        mean_indgain_NB1, mean_indgain_NB2
+    ):
+    """
+    Performs statistical test to determine if the two algorithms are the "same"
+    aka equal distribution
+    """
+    levene1 = levene(mean_indgain_dgea1, mean_indgain_dgea2)
+    ttest1 = ttest_ind(mean_indgain_dgea1, mean_indgain_dgea2)
+    ftest1 = f_oneway(mean_indgain_dgea1, mean_indgain_dgea2)
+    result1 = (levene1, ttest1, ftest1)
+
+    levene2 = levene(mean_indgain_dgea1, mean_indgain_NB1)
+    ttest2 = ttest_ind(mean_indgain_dgea1, mean_indgain_NB1)
+    ftest2 = f_oneway(mean_indgain_dgea1, mean_indgain_NB1)
+    result2 = (levene2, ttest2, ftest2)
+
+    levene3 = levene(mean_indgain_NB1, mean_indgain_NB2)
+    ttest3 = ttest_ind(mean_indgain_NB1, mean_indgain_NB2)
+    ftest3 = f_oneway(mean_indgain_NB1, mean_indgain_NB2)
+    result3 = (levene3, ttest3, ftest3)
+
+    levene4 = levene(mean_indgain_dgea2, mean_indgain_NB2)
+    ttest4 = ttest_ind(mean_indgain_dgea2, mean_indgain_NB2)
+    ftest4 = f_oneway(mean_indgain_dgea2, mean_indgain_NB2)
+    result4 = (levene4, ttest4, ftest4)
+
+    # save statistics
+    folder = os.path.join("results", "runs_best_solutions")
+    os.makedirs(folder, exist_ok=True)
+    relpath = os.path.join(folder, "statistics.txt")
+    with open(relpath, 'w') as txt_file:
+        results = [result1, result2, result3, result4]
+        compares = ["DGEA 1 and DGEA 2", "DGEA 1 and NB 1", "NB 1 and NB 2", "DGEA 2 and NB 2"]
+        for compare, result in zip(compares, results):
+            levene_stat, ttest, ftest = result
+            txt_file.write("Statistics between {}\n".format(compare))
+            txt_file.write("Levene: {}\n".format(levene_stat))
+            txt_file.write("T test: {}\n".format(ttest))
+            txt_file.write("F test: {}\n".format(ftest))
+
+    return result1, result2, result3, result4
+
+def make_boxplot(
+        mean_indgain_dgea1, mean_indgain_dgea2, 
+        mean_indgain_NB1, mean_indgain_NB2
+    ):
+    """
+    """
+    folder = os.path.join("results", "runs_best_solutions")
+    os.makedirs(folder, exist_ok=True)
+    plt.figure(figsize=(10, 3))
+    plt.boxplot([mean_indgain_dgea1, mean_indgain_dgea2, mean_indgain_NB1,
+                 mean_indgain_NB2])
+    plt.xticks([1, 2, 3, 4], ['DGEA - E78', 'DGEA - E26', 'NB - E78', 'NB - E26'], fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.title("Individual gain per algorithm and training set")
+    plt.ylabel('Individual Gain', fontsize=14)
+    plt.savefig(os.path.join(folder, 'boxplot_best10solutions.pdf'), dpi=300)
+    plt.show()
 
 if __name__ == "__main__":
-    filenames = ["newblood_random_training", "best_sols_e7e8.npz", "best_fits_best_solse7e8.csv"]
-    best_sol, sol_maxdistance, sol_mindiff_fit = load_best_sols(*filenames)
-    print("BEST SOLUTION")
-    print(best_sol)
-    print("==================================================================")
-    print("MOST DIFFERENT SOLUTION")
-    print(sol_maxdistance)
-    print("==================================================================")
-    print("SECOND BEST SOLUTION")
-    print(sol_mindiff_fit)
-    print("==================================================================")
+    reps, N_best = 5, 10
+    filenames_dgea1 = ["dgea_dgea_first_e_set", "best_sols_e7e8.npz", "best_fits_best_solse7e8.csv"]
+    filenames_dgea2 = ["dgea_dgea_second_e_set", "best_sols_e2e6.npz", "best_fits_best_solse2e6.csv"]
+    filenames_NB1 = ["dgea_newblood_first_e_set", "best_sols_e7e8.npz", "best_fits_best_solse7e8.csv"]
+    filenames_NB2 = ["dgea_newblood_second_e_set", "best_sols_e2e6.npz", "best_fits_best_solse2e6.csv"]
+
+    print("Loading started")
+    start_load = time.time()
+    best_sol_dgea1, sol_maxdistance_dgea1, best_N_sols_dgea1 = load_best_sols(*filenames_dgea1, N_best)
+    best_sol_dgea2, sol_maxdistance_dgea2, best_N_sols_dgea2 = load_best_sols(*filenames_dgea2, N_best)
+    best_sol_NB1, sol_maxdistance_NB1, best_N_sols_NB1 = load_best_sols(*filenames_NB1, N_best)
+    best_sol_NB2, sol_maxdistance_NB2, best_N_sols_NB2 = load_best_sols(*filenames_NB2, N_best)
+    print("Loading finished in {} minutes".format(round((time.time() - start_load) / 60), 2))
+
+    print("Run best solutions")
+    start_run = time.time()
+    results_dgea1 = run_solutions(filenames_dgea1[0], reps, best_N_sols_dgea1)
+    _, _, mean_indgain_dgea1, best_solution_dgea1, best_indgain_dgea1 = results_dgea1
+
+    results_dgea2 = run_solutions(filenames_dgea2[0], reps, best_N_sols_dgea2)
+    _, _, mean_indgain_dgea2, best_solution_dgea2, best_indgain_dgea2 = results_dgea2
+
+    results_NB1 = run_solutions(filenames_NB1[0], reps, best_N_sols_NB1)
+    _, _, mean_indgain_NB1, best_solution_NB1, best_indgain_NB1 = results_NB1
+
+    results_NB2 = run_solutions(filenames_NB2[0], reps, best_N_sols_NB2)
+    _, _, mean_indgain_NB2, best_solution_NB2, best_indgain_NB2 = results_NB2
+    print("Run best solutions finished in {} minutes".format(round((time.time() - start_run) / 60), 2))
+
+    print("Determine statistics")
+    start_stats = time.time()
+    results_stats = statistical_tests(
+        mean_indgain_dgea1, mean_indgain_dgea2, 
+        mean_indgain_NB1, mean_indgain_NB2
+    )
+    print("Stats finished in {} minutes".format(round((time.time() - start_stats) / 60), 2))
+
+    print("Make boxplot")
+    start_box = time.time()
+    make_boxplot(mean_indgain_dgea1, mean_indgain_dgea2, mean_indgain_NB1, mean_indgain_NB2)
+    print("Boxplot finished in {} minutes".format(round((time.time() - start_box) / 60), 2))
+    
+    print("Total script ran in {} minutes".format(round((time.time() - start_load) / 60, 2)))
+
+    # # run best solution ansd the most different one
+    # results_best_diff_dgea1 = run_solutions("best_and_diff_dgea1", reps, [best_sol_dgea1, sol_maxdistance_dgea1])
+    # _, _, mean_indgain_best_diff_dgea1, best_diff_solution_dgea1, best_diff_indgain_dgea1 = results_best_diff_dgea1
+
+    # results_best_diff_dgea2 = run_solutions("best_and_diff_dgea2", reps, [best_sol_dgea2, sol_maxdistance_dgea2])
+    # _, _, mean_indgain_best_diff_dgea2, best_diff_solution_dgea2, best_diff_indgain_dgea2 = results_best_diff_dgea2
+
+    # results_best_diff_NB1 = run_solutions("best_and_diff_NB1", reps, [best_sol_NB1, sol_maxdistance_NB1])
+    # _, _, mean_indgain_best_diff_NB1, best_diff_solution_NB1, best_diff_indgain_NB1 = results_best_diff_NB1
+
+    # results_best_diff_NB2 = run_solutions("best_and_diff_NB2", reps, [best_sol_NB2, sol_maxdistance_NB2])
+    # _, _, mean_indgain_best_diff_NB2, best_diff_solution_NB2, best_diff_indgain_NB2 = results_best_diff_NB2
+
